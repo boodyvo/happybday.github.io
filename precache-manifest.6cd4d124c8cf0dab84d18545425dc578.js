93dce0f1e0c8fbeadcd4f71a81143c818dfd7abe
Vladyslav Budichenko <boodyvo@gmail.com>
2020-01-10 14:01:31 +0200

    Updates

---
+++ precache-manifest.6cd4d124c8cf0dab84d18545425dc578.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02853b6cf9679d7448c361b7559c56d1",
+    "url": "/boodyvo/happybday.github.io/index.html"
+  },
+  {
+    "revision": "76372a60d49132c51050",
+    "url": "/boodyvo/happybday.github.io/static/css/main.d811afd2.chunk.css"
+  },
+  {
+    "revision": "7d95d89000204c5b8f24",
+    "url": "/boodyvo/happybday.github.io/static/js/2.e0f9afd5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/boodyvo/happybday.github.io/static/js/2.e0f9afd5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "76372a60d49132c51050",
+    "url": "/boodyvo/happybday.github.io/static/js/main.39fcfe0b.chunk.js"
+  },
+  {
+    "revision": "f1c9c488969cf4e91bef",
+    "url": "/boodyvo/happybday.github.io/static/js/runtime-main.3629fec6.js"
+  },
+  {
+    "revision": "2c3d3d3a75f95af005b558d31640cae6",
+    "url": "/boodyvo/happybday.github.io/static/media/balloons.2c3d3d3a.jpg"
+  },
+  {
+    "revision": "2fd6aa39a2f8f0a4ac215aa6805fb16e",
+    "url": "/boodyvo/happybday.github.io/static/media/cup.2fd6aa39.jpg"
+  }
+]);
